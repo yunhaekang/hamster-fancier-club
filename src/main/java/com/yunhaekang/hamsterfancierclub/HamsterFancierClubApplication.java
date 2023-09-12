1a238be2537d3221a2df62d1e0a72c7878e73817
yunhaekang <yunhaekang1@gmail.com>
2023-09-12 16:03:46 +0900

    first commit

---
+++ src/main/java/com/yunhaekang/hamsterfancierclub/HamsterFancierClubApplication.java
@@ -0,0 +1,13 @@
+package com.yunhaekang.hamsterfancierclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamsterFancierClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamsterFancierClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yunhaekang/hamsterfancierclub/HamsterFancierClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunhaekang.hamsterfancierclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamsterFancierClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
